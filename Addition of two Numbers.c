c307941058f2b484f105127576ae24797e4112fa
RakeshAchanta <92287545+RakeshAchanta@users.noreply.github.com>
2021-10-20 10:34:08 +0530

    Add files via upload

---
+++ Addition of two Numbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+/*Addition of two Numbers*/
+{
+	int number1,number2,difference;
+	number1=10;
+	number2=20;
+	difference=number1-number2;
+	printf("Difference of %d and %d is %d",number1,number2,difference);
+}
---
+++ Variable Decleration.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+/*Addition of two Numbers*/
+{
+	int number1,number2,sum;
+	number1=10;
+	number2=20;
+	sum=number1+number2;
+	printf("Sum of %d and %d is %d",number1,number2,sum);
+}
