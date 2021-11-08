bbe8f74d305033a10a2a421b0432c548a694ca40
RakeshAchanta <92287545+RakeshAchanta@users.noreply.github.com>
2021-11-08 10:20:38 +0530

    Add files via upload

---
+++ Relational Operators.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+	int a,b;
+	printf("Enter 'a' value ");
+	scanf("%d",&a);
+	printf("Enter 'b' value ");
+	scanf("%d",&b);
+	printf("%d > %d -->%d\n",a,b,a>b);
+	printf("%d >= %d -->%d\n",a,b,a>=b);
+	printf("%d < %d -->%d\n",a,b,a<b);
+	printf("%d <= %d -->%d\n",a,b,a<=b);
+	printf("%d == %d -->%d\n",a,b,a==b);
+	printf("%d != %d -->%d",a,b,a!=b);
+	
+	
+	
+}
